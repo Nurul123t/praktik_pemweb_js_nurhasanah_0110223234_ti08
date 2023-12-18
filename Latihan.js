416ae2eed97f10555806e624b6e2bb011658fe13
Nurul123t <145987621+Nurul123t@users.noreply.github.com>
2023-12-17 19:51:42 -0800

    Add files via upload

---
+++ Latihan.js
@@ -0,0 +1,54 @@
+// ini variabel dengan let
+let nama = "Nurhasanah"
+console.log(nama)
+
+// ini variabel dengan const
+const firstname = "Fadhlurrohman zuhdi"
+const lastname = "zuhdi"
+console.log(firsname + " " + lastname)
+
+// tipedata string
+const gender ="lk" 
+console.log(typeof gender)
+
+//ini typedata int
+const age = 21
+console.log(age)
+console.log(typeof age)
+
+//tipedata boolean
+const isMarried = false;
+console.log(isMarried)
+console.log(typeof isMarried)
+
+//Operator aritmatika
+const x = 9
+const y = 10
+
+// pertambahan
+const z = x + y
+
+// pengurangan 
+const m = x - y
+
+// perkalian
+const n = x * y
+
+// pembagian
+const c = 10 % 2
+
+console.log(z)
+console.log(m)
+console.log(n)
+console.log(o)
+console.log(c)
+
+//function
+function salam(){
+    let city ="Depok"
+}
+console.log(city)
+
+istrue;
+
+//tipedata boolean
